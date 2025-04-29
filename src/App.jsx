66b655468ce06263a886f24120521271fafd69c2
Brendon Santos de Almeida <almeida.brendon@outlook.com>
2025-04-28 22:21:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Login from "./components/Login";
+
+
+function App() {
+  
+  
+  return (
+    <div>
+      <h1 className="text-top">Tela de Login</h1>
+      <Login/>
+    </div>
+ 
+  );
+}
+
+export default App
---
+++ src/services/firebase.js
@@ -0,0 +1,22 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1PncgYU_ZDx_1VQ7vM_mo87n1iNg_cf8",
+  authDomain: "projeto-leitura-7f7ea.firebaseapp.com",
+  projectId: "projeto-leitura-7f7ea",
+  storageBucket: "projeto-leitura-7f7ea.firebasestorage.app",
+  messagingSenderId: "303309617484",
+  appId: "1:303309617484:web:7adbc8e868a90f5273b7e9"
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa o Auth
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
+
